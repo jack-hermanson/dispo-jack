b0cf4e5b23171d07bd22ac1603337882ff6f6305
Jack Hermanson <jack.hermanson@live.com>
2021-05-21 16:58:04 -0600

    get client side up and running

--- client/src/MainRouter.tsx
+++ client/src/MainRouter.tsx
@@ -1,14 +1,14 @@
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import {Navigation} from "./components/Navigation/Navigation";
 import {Button, Container} from "reactstrap";
+import {Account} from "./components/Account/Account";
 
 export const MainRouter = () => {
     return (
         <BrowserRouter>
             <Navigation />
             <Container className="mb-3">
-                <p>Test</p>
-                <Button color="primary">Test</Button>
+                <Route exact path="/account" component={Account} />
             </Container>
         </BrowserRouter>
     )
