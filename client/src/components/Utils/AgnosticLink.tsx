6bd4dad4a9b2851395bdccbc84c529559f626072
Jack Hermanson <jack.hermanson@live.com>
2021-05-22 18:56:06 -0600

    show account details

---
+++ client/src/components/Utils/AgnosticLink.tsx
@@ -0,0 +1,21 @@
+import React, {Fragment} from "react";
+import {Link} from "react-router-dom";
+
+interface Props {
+    linkType: "internal" | "external";
+    linkText: string;
+    path: string;
+    className?: string;
+}
+
+export const AgnosticLink: React.FC<Props> = ({linkType, linkText, path, className}: Props) => {
+    return (
+        <Fragment>
+            {linkType === "internal" ? (
+                <Link to={path} className={className}>{linkText}</Link>
+            ) : (
+                <a href={path} rel="noreferrer" target="_blank" className={className}>{linkText}</a>
+            )}
+        </Fragment>
+    );
+}
--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,2 +1,10 @@
+export interface KeyValPair {
+    key: string;
+    val: string | number;
+    url?: {
+        path: string;
+        linkType: "internal" | "external";
+    }
+}
 
 export type bsColor = "info" | "success" | "primary" | "warning" | "danger";
