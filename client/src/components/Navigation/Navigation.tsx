d9733bffbea83445771962cb187dbb70748f7d0a
Jack Hermanson <jack.hermanson@live.com>
2021-05-22 19:30:44 -0600

    log out

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,13 +1,29 @@
 import React, {useState} from "react";
-import {Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem} from "reactstrap";
+import {
+    ButtonDropdown,
+    Collapse,
+    Container,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    NavItem
+} from "reactstrap";
 import {NavLink, useHistory} from "react-router-dom";
-import {faHome, faCannabis, faCode, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {faCannabis, faHome, faUserCircle} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon as FA} from "@fortawesome/react-fontawesome";
+import {useStoreActions, useStoreState} from "../../store";
 
 export const Navigation: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const toggle = () => setIsOpen(!isOpen);
     const history = useHistory();
+    const account = useStoreState(state => state.currentUser?.account);
+    const [userBtnOpen, setUserBtnOpen] = useState(false);
+    const logOut = useStoreActions(actions => actions.logOut);
 
     return (
         <Navbar dark className="mb-4 main-navbar px-0" expand="lg">
@@ -24,11 +40,45 @@ export const Navigation: React.FC = () => {
                     </Nav>
                     <Nav navbar style={{marginLeft: "auto"}}>
                         <NavItem>
-                            <NavLink className="nav-link" to="/account"><FA icon={faUserCircle}/> Account</NavLink>
+                            {renderUser()}
                         </NavItem>
                     </Nav>
                 </Collapse>
             </Container>
         </Navbar>
     );
+
+    function renderUserIcon() {
+        return <FA className="me-1" icon={faUserCircle}/>;
+    }
+
+    function renderUser() {
+        return (
+            <NavLink className="nav-link py-0 pe-0" to="/account" onClick={e => {
+                if (account) {
+                    e.preventDefault();
+                }
+            }}>
+                {account ? (
+                    <ButtonDropdown isOpen={userBtnOpen} toggle={() => setUserBtnOpen(open => !open)}>
+                        <DropdownToggle size="sm" color="info" caret>
+                            {renderUserIcon()}
+                            {account.username}
+                        </DropdownToggle>
+                        <DropdownMenu end>
+                            <DropdownItem onClick={() => {
+                                history.push("/account");
+                            }}>My Account</DropdownItem>
+                            <DropdownItem divider/>
+                            <DropdownItem onClick={async () => {
+                                await logOut(account.token!);
+                                history.push("/account");
+                            }}>Log Out</DropdownItem>
+                        </DropdownMenu>
+                    </ButtonDropdown>
+                ) : <span>{renderUserIcon()} Account</span>}
+            </NavLink>
+        )
+    }
+
 }
