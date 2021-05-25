9cebdace6a3075b04bd444dae4511aebd0f05a59
Jack Hermanson <jack.hermanson@live.com>
2021-05-25 10:37:37 -0600

    add strains and strainTypes to store

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,12 +1,22 @@
 import {createStore, createTypedHooks, action, Action, thunk, Thunk} from "easy-peasy";
 import {AccountAndPerson, LoginRequest} from "./data/account";
 import {logIn, logOut} from "./api/account";
+import {StrainRecord, StrainTypeRecord} from "./data/strain";
+import {getStrains, getStrainTypes} from "./api/strain";
 
 interface StoreModel {
     currentUser: AccountAndPerson | undefined;
     setCurrentUser: Action<StoreModel, AccountAndPerson | undefined>;
     logIn: Thunk<StoreModel, LoginRequest>;
     logOut: Thunk<StoreModel, string>;
+
+    strains: StrainRecord[] | undefined;
+    setStrains: Action<StoreModel, StrainRecord[]>;
+    fetchStrains: Thunk<StoreModel>;
+
+    strainTypes: StrainTypeRecord[] | undefined;
+    setStrainTypes: Action<StoreModel, StrainTypeRecord[]>;
+    fetchStrainTypes: Thunk<StoreModel>;
 }
 
 export const store = createStore<StoreModel>({
@@ -21,6 +31,24 @@ export const store = createStore<StoreModel>({
     logOut: thunk(async (actions, token) => {
         await logOut(token);
         actions.setCurrentUser(undefined);
+    }),
+
+    strains: undefined,
+    setStrains: action((state, payload) => {
+        state.strains = payload;
+    }),
+    fetchStrains: thunk(async (actions) => {
+        const strains = await getStrains();
+        actions.setStrains(strains);
+    }),
+
+    strainTypes: undefined,
+    setStrainTypes: action((state, payload) => {
+        state.strainTypes = payload;
+    }),
+    fetchStrainTypes: thunk(async (actions) => {
+        const strainTypes = await getStrainTypes();
+        actions.setStrainTypes(strainTypes);
     })
 });
 
