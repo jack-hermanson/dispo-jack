b11cf754afae6f5e8273ad8736a20ab3b719893c
Jack Hermanson <jack.hermanson@live.com>
2021-05-25 10:31:58 -0600

    add GET requests for strain and strainType

---
+++ client/src/data/strain.ts
@@ -0,0 +1,18 @@
+import {BaseModel} from "./_baseModel";
+
+export interface StrainRequest {
+    name: string;
+    strainTypeId: number;
+    ouncePrice: number;
+    quadPrice: number;
+    eighthPrice: number;
+    gramPrice: number;
+}
+
+export interface StrainRecord extends BaseModel, StrainRequest {}
+
+export interface StrainTypeRequest {
+    name: string;
+}
+
+export interface StrainTypeRecord extends BaseModel, StrainRequest {}
