d538fe34596b9bf6aed5cfb8ea496260e6c60ffc
Jack Hermanson <jack.hermanson@live.com>
2021-05-26 00:14:03 -0600

    show badge

--- client/src/components/Strains/Strain.tsx
+++ client/src/components/Strains/Strain.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import {Card, CardHeader} from "reactstrap";
-import {StrainTypeRecord} from "../../data/strain";
+import {Card, CardBody, CardHeader} from "reactstrap";
+import {StrainRecord, StrainTypeRecord} from "../../data/strain";
+import {KeyValListGroup} from "../Utils/KeyValListGroup";
+import {StrainTypeBadge} from "./StrainTypeBadge";
+import {useStoreState} from "../../store";
 
 interface Props {
-    strain: StrainTypeRecord;
+    strain: StrainRecord;
 }
 
 export const Strain: React.FC<Props> = ({strain}: Props) => {
+    const strainType = useStoreState(state => state.strainTypes)?.find(s => s.id === strain.strainTypeId)?.name;
+
     return (
-        <Card>
+        <Card className="mb-4">
             <CardHeader>
-                <h5 className="mb-0">{strain.name}</h5>
+                <h5 className="mb-0">{strain.name} <StrainTypeBadge typeName={strainType || ""} /></h5>
             </CardHeader>
+            <CardBody>
+                <KeyValListGroup flush keyValPairs={[
+                    {key: "Ounce Price", val: `${strain.ouncePrice.toFixed(2)}`}
+                ]} />
+            </CardBody>
         </Card>
     )
 }
