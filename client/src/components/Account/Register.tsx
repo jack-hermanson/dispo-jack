e99e1add384e2689031f9c118fec8332e2a9ea01
Jack Hermanson <jack.hermanson@live.com>
2021-07-17 18:16:07 -0600

    start register page

---
+++ client/src/components/Account/Register.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useStoreState } from "../../stores/_store";
+import { useHistory } from "react-router-dom";
+import { Col, Row } from "reactstrap";
+import { PageHeader } from "jack-hermanson-component-lib";
+
+export const Register: React.FC = () => {
+    const history = useHistory();
+
+    const currentUser = useStoreState(state => state.currentUser);
+
+    useEffect(() => {
+        if (currentUser) {
+            history.replace("/account");
+        }
+    }, [currentUser, history]);
+
+    return (
+        <React.Fragment>
+            <Row>
+                <Col>
+                    <PageHeader title="Register" />
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
+};
