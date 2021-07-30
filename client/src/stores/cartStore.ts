d06d9fb2a04e78ae7c9d35ab7c8276a338fd9f1c
Jack Hermanson <jack.hermanson@live.com>
2021-07-30 12:28:26 -0600

    add actions to cartStore

--- client/src/stores/cartStore.ts
+++ client/src/stores/cartStore.ts
@@ -10,10 +10,48 @@ import { CartBatchClient } from "../clients/CartBatchClient";
 
 export interface CartStoreModel {
     cart: CartRecord | undefined;
+    setCart: Action<StoreModel, CartRecord | undefined>;
+    createCart: Thunk<StoreModel, CartRequest>;
     cartBatches: CartBatchRecord[] | undefined;
+    setCartBatches: Action<StoreModel, CartBatchRecord[] | undefined>;
+    addCartBatch: Action<StoreModel, CartBatchRecord>;
+    createCartBatch: Thunk<StoreModel, CartBatchRequest>;
 }
 
 export const cartStore: CartStoreModel = {
     cart: undefined,
+    setCart: action((state, payload) => {
+        state.cart = payload;
+    }),
+    createCart: thunk(async (actions, payload) => {
+        try {
+            const cart = await CartClient.create(payload);
+            actions.setCart(cart);
+        } catch (error) {
+            actions.addError(error.message);
+            console.error(error.response);
+            throw error;
+        }
+    }),
     cartBatches: undefined,
+    setCartBatches: action((state, payload) => {
+        state.cartBatches = payload;
+    }),
+    addCartBatch: action((state, payload) => {
+        if (state.cartBatches) {
+            state.cartBatches = [...state.cartBatches, payload];
+        } else {
+            state.cartBatches = [payload];
+        }
+    }),
+    createCartBatch: thunk(async (actions, payload) => {
+        try {
+            const cartBatch = await CartBatchClient.create(payload);
+            actions.addCartBatch(cartBatch);
+        } catch (error) {
+            console.error(error.response);
+            actions.addError(error.message);
+            throw error;
+        }
+    }),
 };
